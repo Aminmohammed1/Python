4e821550abcf9881b7dd71fb6bf623b2177e8382
aminPersonal <amin.mohiuddin12@gmail.com>
2025-12-07 00:05:02 +0530

    diffrent types of paramters/ args in methods in python

--- functions.py
+++ functions.py
@@ -18,8 +18,26 @@ message = get_greeting("amin")
 # file.write(message)
 # file.close
 
+# positional args in methods in python
 
 def increment(by, number):
     return number+by
 
 print(increment(number=5, by=2))
+
+
+# optional parameters in a method
+def increment_optional_by_parameter(number, by=1):
+    return number+by
+
+print(increment_optional_by_parameter(5))
+print(increment_optional_by_parameter(5, 3))
+
+# *args
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total = number*total
+    return total
+
+print(multiply(2,3,4,5))    
